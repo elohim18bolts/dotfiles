96f93458efd58f19569860c4650ef2ae95679385
elohim18bolts <fduran@fduran.xyz>
2023-09-10 20:23:57 -0400

    Added lua_ls configuration to lspconfig

--- nvim/after/plugin/lspconfig.lua
+++ nvim/after/plugin/lspconfig.lua
@@ -2,7 +2,22 @@
 local lspconfig = require('lspconfig')
 lspconfig.pyright.setup {}
 lspconfig.tsserver.setup {}
-lspconfig.lua_ls.setup {}
+lspconfig.lua_ls.setup {
+	settings = {
+		runtime = { version = 'LuaJIT', },
+		diagnostics = {
+			globals = { 'vim', 'require' },
+		},
+		workspace = {
+			-- Make the server aware of Neovim runtime files
+			library = vim.api.nvim_get_runtime_file("", true),
+		},
+		-- Do not send telemetry data containing a randomized but unique identifier
+		telemetry = {
+			enable = false,
+		},
+	},
+}
 lspconfig.yamlls.setup {}
 lspconfig.rust_analyzer.setup {
 	-- Server-specific settings. See `:help lspconfig-setup`
