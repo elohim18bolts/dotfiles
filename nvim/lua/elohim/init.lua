0fdb2fe22b9377e1804c7325f144a7f37fc83b36
elohim18bolts <fduran@fduran.xyz>
2024-02-25 12:23:41 -0500

    MOD:neovim some usefull settings

--- nvim/lua/elohim/init.lua
+++ nvim/lua/elohim/init.lua
@@ -1,6 +1,9 @@
 require("elohim.packer")
 require("elohim.remap")
+-- require("elohim.remap")
 -- Show line numbers
 vim.cmd [[set nu rnu]]
-vim.cmd [[set clipboard=unnamedplus]]
+vim.cmd [[set clipboard="unnamed, unnamedplus"]]
+vim.cmd [[set cursorline]]
+vim.cmd [[set scrolloff=8]]
 vim.cmd('autocmd BufRead,BufNewFile user-data set filetype=yaml')
--- nvim/lua/elohim/remap.lua
+++ nvim/lua/elohim/remap.lua
@@ -1,3 +1,4 @@
+vim.keymap.set("n", "<Space>", "<Nop>", { silent = true, remap = false })
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeToggle)
 vim.keymap.set("i", "jk", "<ESC>")
