841782b1782db75c48b773432e9d3d7afc5b348e
elohim18bolts <fduran@fduran.xyz>
2024-04-26 17:07:46 -0400

    MOD: change set for lua api

--- nvim/lua/elohim/init.lua
+++ nvim/lua/elohim/init.lua
@@ -2,8 +2,16 @@ require("elohim.packer")
 require("elohim.remap")
 -- require("elohim.remap")
 -- Show line numbers
-vim.cmd [[set nu rnu]]
-vim.cmd [[set clipboard="unnamed, unnamedplus"]]
-vim.cmd [[set cursorline]]
-vim.cmd [[set scrolloff=8]]
+--vim.cmd [[set nu rnu]]
+--vim.cmd [[set clipboard="unnamedplus"]]
+--vim.cmd [[set cursorline]]
+--vim.cmd [[set scrolloff=15]]
+vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cursorline = true
+vim.opt.scrolloff = 15
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+
 vim.cmd('autocmd BufRead,BufNewFile user-data set filetype=yaml')
