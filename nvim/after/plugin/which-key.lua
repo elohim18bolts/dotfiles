9562d5c9cf2a093dd992c6e8005d84aaccfac798
elohim18bolts <fduran@fduran.xyz>
2024-08-12 19:53:17 -0400

    Which-key: changed to new file format

--- nvim/after/plugin/which-key.lua
+++ nvim/after/plugin/which-key.lua
@@ -1,13 +1,15 @@
 local wk = require('which-key')
-wk.register({
-	f = {
-		name = "file",                               -- optional group name
-		f = { "<cmd>Telescope find_files<cr>", "Find File" }, -- create a binding with label
-		p = { "<cmd>Telescope git_files<cr>", "Find File In Project" },
-		r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" }, -- noremap = false, buffer = 123 }, -- additional options for creating the keymap
-		g = { "<cmd>Telescope grep_string<cr>", "Searches for the string under your cursor" },
-		G = { "<cmd>Telescope live_grep<cr>", "Search for a string in your current working directory" },
-		n = {
+wk.add({
+	mode = { "n", "v" },
+	{
+		{ "<leader> f", group = "File" },
+		{ "<leader>ff", "<cmd>Telescope find_files<cr>",  desc = "Find File" },
+		{ "<leader>fp", "<cmd>Telescope git_files<cr>",   desc = "Find File In Project" },
+		{ "<leader>fr", "<cmd>Telescope oldfiles<cr>",    desc = "Open Recent File" }, -- noremap = false, buffer = 123 }, -- additional options for creating the keymap
+		{ "<leader>fg", "<cmd>Telescope grep_string<cr>", desc = "Searches for the string under your cursor" },
+		{ "<leader>fG", "<cmd>Telescope live_grep<cr>",   desc = "Search for a string in your current working directory" },
+		{
+			"<leader>fn",
 			function()
 				vim.ui.input({
 					prompt = "File Name: ",
@@ -16,8 +18,11 @@ wk.register({
 					vim.cmd(string.format("e %s", input))
 				end
 				)
-			end, "New File" },
-		R = {
+			end,
+			desc = "New File"
+		},
+		{
+			"<leader>fR",
 			function()
 				local bufferName = vim.api.nvim_buf_get_name(0)
 				if string.find(string.format("%s", bufferName), "NvimTree") ~= nil then
@@ -30,35 +35,37 @@ wk.register({
 					vim.cmd(string.format("file %s", input))
 				end
 				)
-			end
-			, "Rename File In Place"
+			end,
+			desc = "Rename File In Place"
+
 		},
-		["1"] = "which_key_ignore", -- special label to hide it in the popup
-	},
-	g = {
-		name = "Git",
-		g = { "<cmd>Git<cr>", "Git Management" },
-		p = { "<cmd>Git push origin<cr>", "Push To Remote Repo" },
-	},
-	b = {
-		name = "Buffer",
-		b = { "<cmd>Telescope buffers<cr>", "Show Opened Buffers" },
-		s = { "<cmd>Telescope current_buffer_fuzzy_find<cr>", "Find In Current Buffer" },
 
 	},
-	w = {
-		name = "Panels",
-		v = { "<cmd>vsplit<cr>", "Split Window Vertical" },
-		z = { "<cmd>split<cr>", "Split Window Horizontal" },
-	},
-	t = {
-		name = "Terminal Or Tabs",
-		t    = { "<cmd>FloatermToggle<cr>", "Toggle Floating Terminal" },
-		n    = { "<cmd>TablineBufferNext<cr>", "Next Tab" },
-		p    = { "<cmd>TablineBufferPrevious<cr>", "Previous Tab" },
-		h    = { "<cmd>TablineToggleShowAllBuffers<cr>", "Toggle Show All Tabs" },
+	{
+		{ "<leader>g",  group = "Git" },
+		{ "<leader>gg", "<cmd>Git<cr>",             desc = "Git Management" },
+		{ "<leader>gp", "<cmd>Git push origin<cr>", desc = "Push To Remote Repo" },
 
 	},
+	{
+		{ "<leader>b",  group = "Buffer" },
+		{ "<leader>bb", "<cmd>Telescope buffers<cr>",                   desc = "Show Opened Buffers" },
+		{ "<leader>bs", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Find In Current Buffer" },
+	},
+	{
+		{ "<leader>w",  group = "Panels" },
+		{ "<leader>wv", "<cmd>vsplit<cr>", desc = "Split Window Vertical" },
+		{ "<leader>wz", "<cmd>split<cr>",  desc = "Split Window Horizontal" },
+
+	},
+	{
+		{ "<leader>t",  group = "Terminals And Tabs" },
+		{ "<leader>tt", "<cmd>FloatermToggle<cr>",              desc = "Toggle Floating Terminal" },
+		{ "<leader>tn", "<cmd>TablineBufferNext<cr>",           desc = "Next Tab" },
+		{ "<leader>tp", "<cmd>TablineBufferPrevious<cr>",       desc = "Previous Tab" },
+		{ "<leader>th", "<cmd>TablineToggleShowAllBuffers<cr>", desc = "Toggle Show All Tabs" },
+
+	},
 
 
-}, { prefix = "<leader>" })
+})
