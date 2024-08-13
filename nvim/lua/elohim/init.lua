467be889beec960174b22ca4e1874ce5eac20ada
elohim18bolts <fduran@fduran.xyz>
2024-08-13 19:29:10 -0400

    nvim: set end of line off

--- nvim/lua/elohim/init.lua
+++ nvim/lua/elohim/init.lua
@@ -11,7 +11,10 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.cursorline = true
 vim.opt.scrolloff = 15
 vim.opt.foldmethod = "expr"
+vim.opt.foldlevel = 20
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.binary = true
+vim.opt.endofline = false
 
 
 vim.cmd('autocmd BufRead,BufNewFile user-data set filetype=yaml')
